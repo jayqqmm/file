2a8457abc370d1f64f8d400f8718cf5f9d6ac335
jayqqmm <jayqqmm@163.com>
2019-01-21 21:23:31 -0700

    useful work

--- text.c
+++ text.c
@@ -14,10 +14,12 @@ int main(int argc, char* argv[]) {
     scanf("%s", ( char *)i);
 
     printf("str = %s\n", str);
+    printf("%s\n", str + 3);
     printf("i[0] = 0x%x\n", i[0]);
     printf("i[1] = 0x%x\n", i[1]); printf("str = %p\n", str);
-    printf("i = %p\n", i);
-    printf("sizeof(int) = %ld\n", sizeof(int));
+
+    scanf("%x %x", (unsigned *)str, ((unsigned *)(str + 4)));
+    printf("str = %s\n", str);
 
     return 0;
 }
